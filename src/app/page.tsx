995e51766acf43d2fff4fd21a053da2bb1a74f75
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2024-04-02 23:22:17 +0000

    feat: stop load_image from running twice

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { invoke } from "@tauri-apps/api/tauri";
 import { readBinaryFile } from "@tauri-apps/api/fs";
@@ -9,27 +9,33 @@ import Rose from "./components/rose";
 import RoseImage from "./components/dynamic_image";
 
 export default function Home() {
+    const is_image_loaded = useRef(false);
+
     const [image_opacity, setImageOpacity] = useState<number>(0);
     const [image, setImage] = useState<[string, [number, number]] | null>(null);
 
     function load_image() {
-        invoke<[string, [number, number]] | null>("get_image").then(
-            image => {
-                if (image !== null) {
-                    const path = image[0];
-                    const dimensions = image[1];
+        if (is_image_loaded.current == false) {
+            invoke<[string, [number, number]] | null>("get_image").then(
+                image_data => {
+                    if (image_data !== null) {
+                        const path = image_data[0];
+                        const dimensions = image_data[1];
 
-                    readBinaryFile(path).then(
-                        (contents) => {
-                            const blob = new Blob([contents], { type: "image/png" });
-                            setImage([URL.createObjectURL(blob), dimensions]);
+                        readBinaryFile(path).then(
+                            (contents) => {
+                                const blob = new Blob([contents], { type: "image/png" });
+                                setImage([URL.createObjectURL(blob), dimensions]);
 
-                            setTimeout(() => setImageOpacity(1), 1000);
-                        }
-                    ).catch(console.error);
+                                setTimeout(() => setImageOpacity(1), 1000);
+                            }
+                        ).catch(console.error);
+                    }
                 }
-            }
-        ).catch(console.error);
+            ).catch(console.error);
+
+            is_image_loaded.current = true;
+        }
     }
 
     useEffect(() => load_image(), []);
