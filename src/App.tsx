ae08b1fd5e6525c5771dfad7fde293198adcc563
Ananas <ananas@ananas.moe>
2024-06-20 09:18:41 +0200

    chore: remove unnecessary React.DepList

--- src/App.tsx
+++ src/App.tsx
@@ -78,7 +78,7 @@ export default function Home() {
             return () => {
                 unlisten.then(f => f());
             };
-        }, [eventName, handler]);
+        }, []);
     };
 
     useEffect(() => load_image());
