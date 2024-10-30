b10df77d9c8b6d114fa7871ea63a5ae84fae0b69
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2024-10-30 00:01:46 +0000

    fix: buggy pan reset animation #12

--- src/zoom_pan.rs
+++ src/zoom_pan.rs
@@ -2,6 +2,7 @@ use std::time::{Duration, Instant};
 
 use eframe::egui::{Context, Pos2, Response, Vec2};
 use log::debug;
+use rand::Rng;
 
 /// Struct that controls the zoom and panning of the image.
 pub struct ZoomPan {
@@ -11,7 +12,8 @@ pub struct ZoomPan {
     pan_offset: Vec2,
     pan_offset_before_reset: Option<Vec2>,
     drag_start: Option<Pos2>,
-    reset_pan_offset_timer: Option<Instant>
+    reset_pan_offset_timer: Option<Instant>,
+    reset_pan_animation_id: u32
 }
 
 impl ZoomPan {
@@ -23,8 +25,14 @@ impl ZoomPan {
             is_panning: false,
             pan_offset: Vec2::ZERO,
             pan_offset_before_reset: None,
-            reset_pan_offset_timer: None
+            reset_pan_offset_timer: None,
+            reset_pan_animation_id: 0
         }
+    }
+
+    pub fn update(&mut self, ctx: &Context) {
+        // Reset pan offset when scheduled.
+        self.pan_reset_update(ctx);
     }
 
     // Method to handle zoom input (scrolling)
@@ -78,6 +86,13 @@ impl ZoomPan {
         }
     }
 
+    pub fn get_transformation(&self, image_size: Vec2, image_position: Pos2) -> (Vec2, Pos2) {
+        let scaled_size = image_size * self.zoom_factor;
+        let image_position = image_position - scaled_size * 0.5 + self.pan_offset;
+
+        (scaled_size, image_position)
+    }
+
     pub fn is_pan_out_of_bounds(&mut self, image_size: Vec2) -> bool {
         // TODO: Make this customizable somehow when we add configuration.
         let bounds_to_not_exceed = (image_size / 1.5) * self.zoom_factor / 1.5;
@@ -93,15 +108,19 @@ impl ZoomPan {
         return false;
     }
 
-    pub fn schedule_pan_reset(&mut self) {
-        if self.reset_pan_offset_timer.is_none() {
-            self.reset_pan_offset_timer = Some(Instant::now());
+    pub fn has_been_messed_with(&mut self) -> bool {
+        if self.zoom_factor != 1.0 {
+            true
+        } else if self.pan_offset != Vec2::ZERO {
+            true
+        } else {
+            false
         }
     }
+}
 
-    pub fn update(&mut self, ctx: &Context) {
-        debug!("UPDATE!");
-        // Reset pan offset when scheduled.
+impl ZoomPan {
+    fn pan_reset_update(&mut self, ctx: &Context) {
         if let Some(timer) = self.reset_pan_offset_timer {
             if timer.elapsed() >= Duration::from_millis(300) {
                 let mut pan_offset_x = 0.0 as f32;
@@ -116,39 +135,37 @@ impl ZoomPan {
 
                 let pan_offset_animated = Vec2::new(
                     egui_animation::animate_eased(
-                        ctx, "pan_offset_x", pan_offset_x, 0.5, simple_easing::cubic_in_out
+                        ctx, 
+                        format!("pan_offset_x_{}", self.reset_pan_animation_id), 
+                        pan_offset_x, 
+                        0.5, 
+                        simple_easing::cubic_in_out
                     ),
                     egui_animation::animate_eased(
-                        ctx, "pan_offset_y", pan_offset_y, 0.5, simple_easing::cubic_in_out
+                        ctx, 
+                        format!("pan_offset_y_{}", self.reset_pan_animation_id), 
+                        pan_offset_y, 
+                        0.5, 
+                        simple_easing::cubic_in_out
                     )
                 );
 
                 self.pan_offset = pan_offset_animated;
 
-                debug!("--> {}", pan_offset_x);
-
                 if self.pan_offset == Vec2::ZERO {
+                    debug!("Pan offset resetting is done.");
                     self.reset_pan_offset_timer = None;
-                    self.pan_offset_before_reset = None
+                    self.pan_offset_before_reset = None;
                 }
             }
         }
     }
 
-    pub fn get_transformation(&self, image_size: Vec2, image_position: Pos2) -> (Vec2, Pos2) {
-        let scaled_size = image_size * self.zoom_factor;
-        let image_position = image_position - scaled_size * 0.5 + self.pan_offset;
-
-        (scaled_size, image_position)
-    }
-
-    pub fn has_been_messed_with(&mut self) -> bool {
-        if self.zoom_factor != 1.0 {
-            true
-        } else if self.pan_offset != Vec2::ZERO {
-            true
-        } else {
-            false
+    pub fn schedule_pan_reset(&mut self) {
+        if self.reset_pan_offset_timer.is_none() {
+            self.reset_pan_offset_timer = Some(Instant::now());
+            self.reset_pan_animation_id = rand::thread_rng().gen::<u32>();
+            debug!("Pan offset reset has been scheduled.");
         }
     }
 }
