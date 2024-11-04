3bae4f34505228883f52603ee31c39dac631b7b9
Ananas <ananas@ananas.moe>
2024-11-04 18:12:13 +0100

    feat: add svg to "select_image"

--- src/files.rs
+++ src/files.rs
@@ -5,7 +5,7 @@ use crate::image::Image;
 
 pub fn select_image() -> Result<Image, Error> {
     let image_path = FileDialog::new()
-        .add_filter("images", &["png", "jpeg", "jpg", "webp", "gif"])
+        .add_filter("images", &["png", "jpeg", "jpg", "webp", "gif", "svg"])
         .pick_file();
 
     let image_or_error = match image_path {
