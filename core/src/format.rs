02025a2712b951b66556f0f0854fab625e2ae037
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2025-12-14 20:27:13 +0000

    feat: add display to image format for roseate-core

--- core/src/format.rs
+++ core/src/format.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 #[derive(Clone, Debug, PartialEq)]
 pub enum ImageFormat {
     Png,
@@ -5,4 +7,16 @@ pub enum ImageFormat {
     Svg,
     Gif,
     Webp
+}
+
+impl Display for ImageFormat {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ImageFormat::Png => write!(f, "PNG (Portable Network Graphics)"),
+            ImageFormat::Jpeg => write!(f, "JPEG (Joint Photographic Experts Group)"),
+            ImageFormat::Svg => write!(f, "SVG (Scalable Vector Graphics)"),
+            ImageFormat::Gif => write!(f, "GIF (Graphics Interchange Format)"),
+            ImageFormat::Webp => write!(f, "WEBP (Web Picture)"),
+        }
+    }
 }
