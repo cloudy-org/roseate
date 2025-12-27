278c00708a68427e57370fba34ff71d8f6fe14ff
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2025-12-27 04:10:13 +0000

    feat: add soft-binds dep, improve key bind config key names

--- app/src/config/models/key_binds.rs
+++ app/src/config/models/key_binds.rs
@@ -1,89 +1,33 @@
 use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize, Default, Hash)]
+#[derive(Serialize, Deserialize, Hash)]
 pub struct KeyBinds {
-    #[serde(default)]
-    pub info_box: InfoBoxBinds,
-    #[serde(default)]
-    pub about_box: AboutBoxBinds,
-    #[serde(default)]
-    pub image: ImageBinds,
-    #[serde(default)]
-    pub ui_controls: UIControlsBinds
+    #[serde(default = "show_image_info")]
+    pub show_image_info: String,
+    #[serde(default = "show_extra_image_info")]
+    pub show_extra_image_info: String,
+    #[serde(default = "show_app_about")]
+    pub show_app_about: String,
+    #[serde(default = "reset_viewport")]
+    pub reset_viewport: String,
+    #[serde(default = "show_ui_controls")]
+    pub show_ui_controls: String
 }
 
+fn show_image_info() -> String { "I".into() }
+fn show_extra_image_info() -> String { "CTRL+I".into() }
+fn show_app_about() -> String { "CTRL+A".into() }
+fn reset_viewport() -> String { "R".into() }
+fn show_ui_controls() -> String { "C".into() }
 
-#[derive(Serialize, Deserialize, Hash)]
-pub struct InfoBoxBinds {
-    #[serde(default = "info_box_toggle")]
-    pub toggle: String,
-}
-
-impl Default for InfoBoxBinds {
+impl Default for KeyBinds {
     fn default() -> Self {
         Self {
-            toggle: info_box_toggle()
+            show_image_info: show_image_info(),
+            show_extra_image_info: show_extra_image_info(),
+            show_app_about: show_app_about(),
+            reset_viewport: reset_viewport(),
+            show_ui_controls: show_ui_controls()
         }
     }
-}
-
-fn info_box_toggle() -> String {
-    "I".to_string()
-}
-
-
-#[derive(Serialize, Deserialize, Hash)]
-pub struct AboutBoxBinds {
-    #[serde(default = "about_box_toggle")]
-    pub toggle: String,
-}
-
-impl Default for AboutBoxBinds {
-    fn default() -> Self {
-        Self {
-            toggle: about_box_toggle()
-        }
-    }
-}
-
-fn about_box_toggle() -> String {
-    "A".to_string()
-}
-
-
-#[derive(Serialize, Deserialize, Hash)]
-pub struct ImageBinds {
-    #[serde(default = "image_reset_pos")]
-    pub reset_pos: String,
-}
-
-impl Default for ImageBinds {
-    fn default() -> Self {
-        Self {
-            reset_pos: image_reset_pos()
-        }
-    }
-}
-
-fn image_reset_pos() -> String {
-    "R".to_string()
-}
-
-
-#[derive(Serialize, Deserialize, Hash)]
-pub struct UIControlsBinds {
-    #[serde(default = "ui_controls_toggle")]
-    pub toggle: String,
-}
-
-impl Default for UIControlsBinds {
-    fn default() -> Self {
-        Self {
-            toggle: ui_controls_toggle()
-        }
-    }
-}
-
-fn ui_controls_toggle() -> String {
-    "C".to_string()
 }
