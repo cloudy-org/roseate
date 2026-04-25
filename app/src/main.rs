ce33a805919b4cdb056d8ca029ce76817896c45c
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2026-04-25 14:28:19 +0100

    feat: use `default_with_fallbacks` instead

--- app/src/main.rs
+++ app/src/main.rs
@@ -6,7 +6,7 @@ use cirrus_authors::Authors;
 use cirrus_clap_cli::EditArgs;
 use cirrus_edit::{Preference, open_editor};
 use cirrus_egui::{config_manager::{ConfigManager}, notifier::Notifier, styling::Styling};
-use cirrus_theming::{colour::Colour, fallbacks::ThemeFallbacks, manager::ThemeManager};
+use cirrus_theming::{colour::Colour, fallbacks::{ThemeFallbacks, ThemePalletFallbacks}, manager::ThemeManager};
 use config::config::Config;
 use env_logger::Builder;
 use image_handler::{ImageHandler};
@@ -191,10 +191,15 @@ fn main() -> eframe::Result {
         &mut notifier,
     );
 
+    let theme_fallbacks = ThemeFallbacks {
+        pallet: ThemePalletFallbacks {
+            accent_colour: Colour::from_hex(0xe05f78)
+        },
+    };
+
     // the theme manager may move into the app 
     // struct in the future as it matures with more features
-    let theme_manager = ThemeManager::default()
-        .set_fallbacks(ThemeFallbacks { accent_colour: Colour::from_hex(0xe05f78) })
+    let theme_manager = ThemeManager::default_with_fallbacks(theme_fallbacks)
         .get_theme_from_env()
         .get_theme_from_system();
 
