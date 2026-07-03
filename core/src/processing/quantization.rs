933993cd2967a68bead0401c9b0045c837c956a9
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2026-07-03 15:29:55 +0100

    feat(core): switch to bit shift

--- core/src/processing/quantization.rs
+++ core/src/processing/quantization.rs
@@ -14,7 +14,7 @@ pub fn squish_pixels_to_u8(pixels: &Pixels) -> Vec<u8> {
         ),
         Pixels::U16(pixels) => pixels
             .iter()
-            .map(|&pixel| (pixel / 257) as u8)
+            .map(|&pixel| (pixel >> 8) as u8)
             .collect(),
         Pixels::F32(pixels) => pixels.iter()
             .map(|&pixel| (pixel.clamp(0.0, 1.0) * 255.0).round() as u8)
